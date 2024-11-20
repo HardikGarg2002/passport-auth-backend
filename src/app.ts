01b970579d28af70904d5dc78420a11393171da8
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-11-20 12:22:39 +0530

    creating server using express

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import dbutils from '@hardikgarg2002/mongodb_utils';
+
+export default function createServer() {
+  dbutils.initMongoDB();
+  const app = express().use(express.json()).use(cors()).use(helmet());
+  return app;
+}
+
+export function destroyApp(event?: string) {
+  console.log('destroying app on event:', event);
+  try {
+    dbutils.disconnectMongoDB();
+    process.exit(0);
+  } catch (error) {
+    console.warn('Error while destroying app', error);
+  }
+}
+
+process.on('SIGINT', () => destroyApp('SIGINT'));
+process.on('SIGTERM', () => destroyApp('SIGTERM'));
+process.on('SIGQUIT', () => destroyApp('SIGQUIT'));
+process.on('SIGUSR2', () => destroyApp('SIGUSR2'));
+process.on('exit', () => console.log('exit called'));
+process.on('uncaughtException', (err) => {
+  console.warn('debug', 'Uncaught exception', err);
+  // destroyApp('uncaughtException');
+});
+process.on('unhandledRejection', (reason, promise) => {
+  console.warn('debug', 'Unhandled Rejection at:', promise, 'reason:', reason);
+  // destroyApp('unhandledRejection');
+});
