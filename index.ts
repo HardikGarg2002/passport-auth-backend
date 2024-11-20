d4846e8a8c37ff0f76060a84cbf4d08f1ed68da4
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-11-20 12:22:14 +0530

    setting up express connection

---
+++ index.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+import dbutils from '@hardikgarg2002/mongodb_utils';
+import createServer from './src/app';
+
+const app = createServer();
+const port = process.env.PORT;
+const environment = process.env.NODE_ENV;
+const service = process.env.APP_SERVICE;
+const appVersion = process.env.APP_VERSION;
+
+app.get('/', (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'text/html');
+  const response = {
+    message: 'Welcome to iTuple Product Service',
+    environment,
+    service,
+    appVersion,
+    dbconnected: dbutils.isMongoDBConnected(),
+  };
+
+  res.send(response);
+});
+// default end point
+app.use('*', (req, res) => {
+  res.status(404).send({
+    error: 'AUTH404: Could not find the page requested by you',
+  });
+});
+
+app.listen(port, () => {
+  console.log('App listening on port environment/ service is ', port, environment, service);
+});
