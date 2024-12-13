ae800ed1d3432ea9241ff1afe672d59ceb96bf76
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-12-13 16:30:00 +0530

    feat: user model setup

---
+++ src/model.ts
@@ -0,0 +1,77 @@
+import { model, Schema } from 'mongoose';
+import { IAuthUser } from './user.js';
+
+const authUserSchema = new Schema<IAuthUser>({
+  name: {
+    type: String,
+    trim: true,
+  },
+  password: {
+    type: String,
+    select: false,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+  },
+  mobile: {
+    type: String,
+    trim: true,
+  },
+  access_token: {
+    type: String,
+    select: false,
+  },
+  refresh_token: {
+    type: String,
+    select: false,
+  },
+  is_active: {
+    type: Boolean,
+    default: false,
+  },
+  role: {
+    type: Object,
+  },
+
+  is_verified: {
+    type: Boolean,
+    default: false,
+  },
+  is_deleted: {
+    type: Boolean,
+    default: false,
+  },
+  is_logged_in: {
+    type: Boolean,
+    default: false,
+  },
+  last_login_at: {
+    type: Date,
+    select: false,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+  updated_by: {
+    type: Schema.Types.ObjectId,
+    ref: 'auth_user',
+    select: false,
+  },
+  created_by: {
+    type: Schema.Types.ObjectId,
+    ref: 'auth_user',
+    select: false,
+  },
+});
+
+export default model<IAuthUser>('auth_user_test', authUserSchema);
